062cd17a14de0d63d6f60985bdc53c19bc2248ec
Kristopher Krasnosky <kkrasnosky@gmail.com>
2022-06-11 05:18:49 +0000

    updated defs

---
+++ include/acoustic_msgs_tools/defs.h
@@ -0,0 +1,16 @@
+#ifndef AMT_DEFS_H
+#define AMT_DEFS_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+///
+///  Namespace stuff
+///
+#define NS_HEAD namespace acoustic_msgs_tools {
+
+#define NS_FOOT }
+
+
+#endif // AMT_DEFS_H
